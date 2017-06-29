92606c2afba272516e1af5ed4609824846e4b2cd
ErickLONG <834424581@qq.com>
2017-06-29 09:12:24 +0800

    Initial commit

---
+++ app/urls.py
@@ -0,0 +1,9 @@
+#！/usr/bin/env python
+# -*- coding:utf-8 -*-
+from django.conf.urls import url
+from . import views
+
+urlpatterns = [
+    url(r'moments_input',views.moments_input),
+    url(r'',views.welcome),
+]
---
+++ app/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+from django.http import HttpResponseRedirect,HttpResponse
+import os
+from app.forms import MomentForm
+from django.core.urlresolvers import reverse
+from django.shortcuts import render
+
+# Create your views here.
+def moments_input(request):
+    if request.method =='POST':
+        form = MomentForm(request.POST)
+        if form.is_valid():
+            moment = form.save()
+            return HttpResponseRedirect(reverse("app.views.welcome"))
+    else:
+        form = MomentForm()
+    PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    return render(request,os.path.join(PROJECT_ROOT,'app/templates','moments_input.html'),{'form':form})
+
+def welcome(request):
+
+    return HttpResponse('HELLO')
